cda5d13ea446b11d4fb925020be747881982e32b
TQuynh109 <thaoquynh0109@gmail.com>
2025-04-15 13:58:20 +0700

    update

--- src/kblam/models/llama3_model.py
+++ src/kblam/models/llama3_model.py
@@ -44,6 +44,8 @@ from transformers.models.llama.modeling_llama import (
     _CONFIG_FOR_DOC,
     LLAMA_INPUTS_DOCSTRING,
     LLAMA_START_DOCSTRING,
+    LlamaDynamicNTKScalingRotaryEmbedding,
+    LlamaLinearScalingRotaryEmbedding,
     LlamaMLP,
     LlamaPreTrainedModel,
     LlamaRMSNorm,
@@ -52,10 +54,6 @@ from transformers.models.llama.modeling_llama import (
     repeat_kv,
 )
 
-from transformers.models.llama.modeling_llama_rotary import (
-    LlamaDynamicNTKScalingRotaryEmbedding,
-    LlamaLinearScalingRotaryEmbedding,
-)
 
 from transformers.utils import (
     add_start_docstrings,
